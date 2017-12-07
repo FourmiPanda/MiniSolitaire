3a7c6b7dd1131edca8a46212dc419c79d53ddbfb
FourmiPanda <ismail.el-khantache@etu.univ-nantes.fr>
2017-12-07 22:22:12 +0100

    Modification esthetique

--- vue/vue.php
+++ vue/vue.php
@@ -21,9 +21,9 @@ class Vue{
       <title>Page d'accueil</title>
 
     </head>
-    <body background="vue/img/fondAccueil.jpg">
+    <body background="vue/img/fondNEWST.jpg" style="background-repeat:no-repeat;background-size: 100%;">
       <?php
-      var_dump($_POST);
+      //var_dump($_POST);
       // var_dump($_GET);
       // var_dump($_SESSION);
       ?>
@@ -88,7 +88,7 @@ class Vue{
       <link rel="stylesheet" href="vue/css/plateau.css" />
       <title>Page de jeu</title>
     </head>
-    <body background="vue/img/fond.jpg">
+    <body background="vue/img/fondNEWST.jpg" style="background-repeat:no-repeat;background-size: 100%;">
 
       <!-- <div style="float:left;"><img src="vue/img/logo.png"></div> -->
 
@@ -123,7 +123,7 @@ class Vue{
       }
       </script>
       <!-- HEURE -->
-      <center>
+      <div class="plateau">
 
         <!-- AFFICHAGE DU PLATEAU-->
 
@@ -136,7 +136,7 @@ class Vue{
             echo "<tr>";
             for($i = 0;$i<7;$i++){
               if($_SESSION['plateauFlorianIsmael']->isCase($j,$i)==1){
-                echo "<td><div class='blur'><a href='index.php?x=";
+                echo "<td><div class='blur'><a method='post' href='index.php?x=";
                 echo $j;
                 echo "&y=";
                 echo $i;
@@ -161,21 +161,167 @@ class Vue{
         <!-- FIN AFFICHAGE DU PLATEAU-->
 
 
-      </center>
+      </div>
       <?php
       if($_SESSION['plateauFlorianIsmael']->isLastMove()){
 
-        echo "<div style='float:right;'>";
-        echo "<form method='post' action='index.php'>";
-        echo "<img alt='recall' src='vue/img/retour.png' height='25' width='25' />";
-        echo "<input type='submit' name='soumettre' value='reCall'/>";
+        echo "<div id='recall'>";
+        echo "<form id='recallForm' method='post' action='index.php'>";
         echo "</form>";
         echo "</div>";
+        echo "<p>";
+        echo "<button id='button' class='BT-OH-BR-R6-NF-FH-FP-TU-PT' type='submit' formmethod='post' name='soumettre' value='reCall' form='recallForm'>";
+        echo "<canvas id='canvas'></canvas>";
+        echo "<hover></hover>";
+        echo "<span style='color:white;'>RETOUR EN ARRIERE</span>";
+
+        echo "</button>";
+
+        echo "</p>";
+
 
 
       }
 
       ?>
+
+
+      <!-- Script de l'aspect graphique du bouton retour en arriere -->
+      <script type="text/javascript">
+      (function() {
+        var lastTime = 0;
+        var vendors = ['ms', 'moz', 'webkit', 'o'];
+        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+          window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+          window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+          || window[vendors[x]+'CancelRequestAnimationFrame'];
+        }
+
+        if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+          var currTime = new Date().getTime();
+          var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+          var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+          timeToCall);
+          lastTime = currTime + timeToCall;
+          return id;
+        };
+
+        if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+          clearTimeout(id);
+        };
+      }());
+
+
+      (function() {
+
+        // Get the buttons.
+        var startBtn = document.getElementById('button');
+        /*var resetBtn = document.getElementById('resetBtn');*/
+        // A variable to store the requestID.
+        var requestID;
+        // Canvas
+        var canvas = document.getElementById('canvas');
+        // 2d Drawing Context.
+        var ctx = canvas.getContext('2d');
+
+        // Variables to for the drawing position and object.
+        var posX = 0;
+        var W = 246;
+        var H = 60;
+        var circles = [];
+
+        //Get canvas size
+        canvas.width = 246;
+        canvas.height = 60;
+
+        // Animate.
+        function animate() {
+          requestID = requestAnimationFrame(animate);
+          //Fill canvas with black color
+          //ctx.globalCompositeOperation = "source-over";
+          ctx.fillStyle = "rgba(0,0,0,0.15)";
+          ctx.fillRect(0, 0, W, H);
+
+          //Fill the canvas with circles
+          for(var j = 0; j < circles.length; j++){
+            var c = circles[j];
+
+            //Create the circles
+            ctx.beginPath();
+            ctx.arc(c.x, c.y, c.radius, 0, Math.PI*2, false);
+            ctx.fillStyle = "rgba("+c.r+", "+c.g+", "+c.b+", 0.5)";
+            ctx.fill();
+
+            c.x += c.vx;
+            c.y += c.vy;
+            c.radius -= .02;
+
+            if(c.radius < 0)
+            circles[j] = new create();
+          }
+
+
+
+        }
+
+        //Random Circles creator
+        function create() {
+
+          //Place the circles at the center
+
+          this.x = W/2;
+          this.y = H/2;
+
+
+          //Random radius between 2 and 6
+          this.radius = 2 + Math.random()*3;
+
+          //Random velocities
+          this.vx = -5 + Math.random()*10;
+          this.vy = -5 + Math.random()*10;
+
+          //Random colors
+          this.r = Math.round(Math.random())*255;
+          this.g = Math.round(Math.random())*255;
+          this.b = Math.round(Math.random())*255;
+        }
+
+        for (var i = 0; i < 500; i++) {
+          circles.push(new create());
+        }
+
+        // Event listener for the start button.
+        startBtn.addEventListener('mouseover', function(e) {
+          e.preventDefault();
+
+          // Start the animation.
+          requestID = requestAnimationFrame(animate);
+        });
+
+
+        // Event listener for the stop button.
+        startBtn.addEventListener('mouseout', function(e) {
+          e.preventDefault();
+
+          // Stop the animation;
+          cancelAnimationFrame(requestID);
+
+          e.preventDefault();
+
+          // Reset the X position to 0.
+          posX = 0;
+
+          // Clear the canvas.
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+          // Draw the initial box on the canvas.
+          ctx.fillRect(posX, 0, boxWidth, canvas.height);
+
+        });
+      }());
+      </script>
 
     </body>
     </html>
